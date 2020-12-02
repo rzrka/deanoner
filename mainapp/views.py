148b9ad83208db0b098a183014d0d92bfd8e515d
rzrka <60391129+rzrka@users.noreply.github.com>
2020-12-03 01:08:25 +0300

    Deanoner02122020 (#2)
    * edit2
    
    * edit3
    
    * edit4
    
    Co-authored-by: daniil <daniil1@daniil.Dlink>

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, "mainapp/index.html")
+    title = "главная"
+    blocks = [
+        {
+            'name': 'Search in social network Vkontakte',
+            'discription' : 'More information',
+            'button_name' : 'Перейти',
+            'url':'/social_vk/',
+        },
+        {
+            'name': 'Search in social network Instagramm',
+            'discription' : 'More information',
+            'button_name' : 'Перейти',
+            'url':'/social_inst/',
+        },
+        {
+            'name': 'Search in social network Odnoklassniki',
+            'discription' : 'More information',
+            'button_name' : 'Перейти',
+            'url':'/social_ok/',
+        },
+    ]
+    content = {"title": title, "blocks": blocks}
+    return render(request, "mainapp/index.html", content)
 
 
 def social_inst(request):
