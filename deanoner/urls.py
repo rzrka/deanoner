cf2a6425d5821f854b180da3417c79b1d40f4466
daniil <daniil1@daniil.Dlink>
2020-12-02 11:22:09 +0300

    edit2

--- deanoner/urls.py
+++ deanoner/urls.py
@@ -5,8 +5,8 @@ import mainapp.views as mainapp
 
 urlpatterns =[
     path("admin/", admin.site.urls),
-    path("", mainapp.main),
-    path("social_inst/", mainapp.social_inst),
-    path("social_ok/", mainapp.social_ok),
-    path("social_vk/", mainapp.social_vk),
+    path("", mainapp.main, name="main"),
+    path("social_inst/", mainapp.social_inst, name="social_inst"),
+    path("social_ok/", mainapp.social_ok, name="social_ok"),
+    path("social_vk/", mainapp.social_vk, name="social_vk"),
 ]
